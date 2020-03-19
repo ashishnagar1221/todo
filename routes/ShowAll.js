3e95a664ff1ca313750995cd7476a672daf7babe
Ashish <ashishnagar1221@gmail.com>
2020-03-20 02:16:47 +0530

    Checkbox done part now working properly

--- routes/ShowAll.js
+++ routes/ShowAll.js
@@ -47,18 +47,43 @@ router.get('/del/:id',function(req,res){
 });
 
 // //TOGGLE DONE AND NOT DONE
-router.get('/check/:id/:done',function(req,res){
-    const id = req.params.id;
-    let checked = !req.params.done;
-    console.log(checked +" "+ typeof(checked));
-    Todo.findByIdAndUpdate(id,{done:checked},function(err){
-        if(err)
-            return res.send(500,err);
-        else{
-            console.log(checked +" "+ typeof(checked));
-            res.redirect('/');
-        }
-    })
-})
+// router.get('/check/:id/:done',function(req,res){
+//     const id = req.params.id;
+//     let checked = !req.params.done;
+//     console.log(checked +" "+ typeof(checked));
+//     Todo.findByIdAndUpdate(id,{done:checked},function(err){
+//         if(err)
+//             return res.send(500,err);
+//         else{
+//             console.log(checked +" "+ typeof(checked));
+//             res.redirect('/');
+//         }
+//     })
+// })
+
+router.post('/check/:id', function(req, res){
+    var mongodb = require('mongodb');
+
+    if(req.body.todo1){
+        console.log("Item Checked");
+        Todo.updateOne({'_id': new mongodb.ObjectID(req.params.id)}, 
+        { $set: {'done': true } },function(err, Todo){
+            if(err) console.log(err);
+            else{
+                console.log("Edited Item: "+ req.params.id);
+            }
+        });
+    }else{
+        console.log("Item Not Checked");
+        Todo.updateOne({'_id': new mongodb.ObjectID(req.params.id)}, 
+        { $set: {'done': false } },function(err, Todo){
+            if(err) console.log(err);
+            else{
+                console.log("Edited Item: "+ req.params.id);
+            }
+        });
+    }
+    res.redirect("/")
+});
 
 module.exports = router;

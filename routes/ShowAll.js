15868039ccf55f4f0979c5bd693c9c2b06d05577
Ashish <ashishnagar1221@gmail.com>
2020-03-20 18:18:54 +0530

    Edit part started ... Some Errors to disoleve

--- routes/ShowAll.js
+++ routes/ShowAll.js
@@ -26,12 +26,22 @@ router.post('/',(req,res)=>{
 
 //Update
 
-// router.get(('/edit/:id'),function(req,res){
-//     const id = req.params.id;
-//     todo.find({},function(err,data){
-//         res.render("changes.ejs",)
-//     })
-// })
+router.get('/edit/:id',function(req,res){
+    const id = req.params.id;
+    Todo.find({},function(err,docs){
+        res.render("changes.ejs",{Todo:docs,ids:id})
+    });
+}).
+post(function(req,res){
+    var mongodb = require('mongodb');
+    Todo.updateOne({'_id': new mongodb.ObjectID(req.params.id)}, 
+        { $set: {'task': req.body.task } },function(err, Todo){
+            if(err) console.log(err);
+            else{
+                console.log("Done");
+            }
+        });
+})
 
 //DELETE
 router.get('/del/:id',function(req,res){
@@ -47,39 +57,25 @@ router.get('/del/:id',function(req,res){
 });
 
 // //TOGGLE DONE AND NOT DONE
-// router.get('/check/:id/:done',function(req,res){
-//     const id = req.params.id;
-//     let checked = !req.params.done;
-//     console.log(checked +" "+ typeof(checked));
-//     Todo.findByIdAndUpdate(id,{done:checked},function(err){
-//         if(err)
-//             return res.send(500,err);
-//         else{
-//             console.log(checked +" "+ typeof(checked));
-//             res.redirect('/');
-//         }
-//     })
-// })
-
 router.post('/check/:id', function(req, res){
     var mongodb = require('mongodb');
 
     if(req.body.todo1){
-        console.log("Item Checked");
+
         Todo.updateOne({'_id': new mongodb.ObjectID(req.params.id)}, 
         { $set: {'done': true } },function(err, Todo){
             if(err) console.log(err);
             else{
-                console.log("Edited Item: "+ req.params.id);
+                console.log("Done");
             }
         });
     }else{
-        console.log("Item Not Checked");
+        
         Todo.updateOne({'_id': new mongodb.ObjectID(req.params.id)}, 
         { $set: {'done': false } },function(err, Todo){
             if(err) console.log(err);
             else{
-                console.log("Edited Item: "+ req.params.id);
+                console.log("Undone");
             }
         });
     }
